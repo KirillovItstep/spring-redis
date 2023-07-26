547af3f182cab996ca862e98778a4e1048d64cf0
kirillovItStep <kirillov.malp@gmail.com>
2023-07-26 16:35:05 +0300

    Spring redis

--- redis-products/src/main/java/org/itstep/ProductRepository.java
+++ redis-products/src/main/java/org/itstep/ProductRepository.java
@@ -9,27 +9,27 @@ import java.util.List;
 @Repository
 public class ProductRepository {
 
-    public static final String HASH_KEY = "Product";
+    public static final String KEY = "Product";
 
     @Autowired
     private RedisTemplate redisTemplate;
 
     public Product save(Product product) {
-        redisTemplate.opsForHash().put(HASH_KEY, product.getId(), product);
+        redisTemplate.opsForHash().put(KEY, product.getId(), product);
         return product;
     }
 
     public List<Product> findAll() {
-        return redisTemplate.opsForHash().values(HASH_KEY);
+        return redisTemplate.opsForHash().values(KEY);
     }
 
     public Product findProductById(int id) {
-        return (Product) redisTemplate.opsForHash().get(HASH_KEY, id);
+        return (Product) redisTemplate.opsForHash().get(KEY, id);
     }
 
 
     public String deleteProduct(int id) {
-        redisTemplate.opsForHash().delete(HASH_KEY, id);
+        redisTemplate.opsForHash().delete(KEY, id);
         return "product removed !!";
     }
 }
