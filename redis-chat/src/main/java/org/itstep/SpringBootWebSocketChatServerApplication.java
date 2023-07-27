2a3d1706968ffe34b28066fe4a7fb0e4edc8fcac
kirillovItStep <kirillov.malp@gmail.com>
2023-07-27 12:56:28 +0300

    Spring redis

---
+++ redis-chat/src/main/java/org/itstep/SpringBootWebSocketChatServerApplication.java
@@ -0,0 +1,13 @@
+package org.itstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketChatServerApplication.class, args);
+	}
+
+}
